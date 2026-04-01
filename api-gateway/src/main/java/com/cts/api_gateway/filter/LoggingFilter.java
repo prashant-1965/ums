298d350d1669daff0b328e8b58371ad00dbdd7f3
Phantom <bhardwajprashant7045@gmail.com>
2026-04-01 09:57:54 +0530

    multi-port registration

--- api-gateway/src/main/java/com/cts/api_gateway/filter/LoggingFilter.java
+++ api-gateway/src/main/java/com/cts/api_gateway/filter/LoggingFilter.java
@@ -6,11 +6,16 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.util.UUID;
+
 @Component
 public class LoggingFilter implements GlobalFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        System.out.println("Request came:  "+exchange.getRequest().getURI());
-        return chain.filter(exchange);
+        String requestId = UUID.randomUUID().toString();
+        System.out.println("Request with id: "+requestId+ "Request came:  "+exchange.getRequest().getURI());
+        return chain.filter(exchange).then(Mono.fromRunnable(()->{
+            System.out.println("After response");
+        }));
     }
 }
